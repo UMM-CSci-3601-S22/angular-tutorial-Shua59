82b1dacd67dd57b67a336429c6f3ea95820db3b9
Joshua Engelkes <enge0479@morris.umn.edu>
2022-02-14 18:57:23 -0600

    Configure CartService to use HttpClient and to get shipping prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,7 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 export class CartService {
   items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -14,5 +20,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
